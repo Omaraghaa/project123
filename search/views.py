f7644abc1ab26ce7a2ac212d5f87b855867a8b6b
MANU JO VARGHESE <mvarghese@deakin.edu.au>
2020-09-12 14:24:02 +1000

    search API for client information phase 2 with views files

---
+++ search/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+
+# Create your views here.
+import requests
+
+from isodate import parse_duration
+
+from django.conf import settings
+from django.shortcuts import render, redirect
+
+
+def index(request):
+    clinical_data = []
+    if request.method == 'POST':
+        search_query = request.POST['search']
+
+        clinic_url = 'https://clinicaltrials.gov/api/query/full_studies'
+
+        clincal_parameters = {
+            'expr': search_query,
+            'min_rnk': 1,
+            'max_rnk': 9,
+            'fmt': 'json'
+        }
+        clinical_request = requests.get(clinic_url, params=clincal_parameters)
+
+        clinical_values = clinical_request.json()['FullStudiesResponse']
+
+        clinical_data = clinical_values['FullStudies']
+
+    context = {
+        'clinical_data': clinical_data
+    }
+
+    return render(request, 'search/index.html', context)
