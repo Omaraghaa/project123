9bf85c94860aec729b16b601f1e62eef5d7d1afe
sttalari <sttalari@deakin.edu.au>
2020-09-14 15:23:28 +1000

    Microsoft API integration - Retrieving JSON Phase 1 (bug fixes)

--- teams_api/views.py
+++ teams_api/views.py
@@ -1,2 +1,74 @@
-#sample data
+from django.shortcuts import render, get_object_or_404
 
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.urls import reverse
+from teams_api.auth_helper import get_sign_in_url, get_token_from_code, store_token, store_user, remove_user_and_token, get_token
+from teams_api.graph_helper import get_user, get_calendar_events
+#import dateutil.parser
+
+# Create your views here.
+def home(request):
+    context = initialize_context(request)
+    return render(request, 'teams/home.html', context)
+    #return HttpResponse("Welcome to the tutorial.")
+
+def initialize_context(request):
+  context = {}
+
+  # Check for any errors in the session
+  error = request.session.pop('flash_error', None)
+
+  if error != None:
+    context['errors'] = []
+    context['errors'].append(error)
+
+  # Check for user in the session
+  context['user1'] = request.session.get('user', {'is_authenticated': False})
+  return context
+
+def sign_in(request):
+  # Get the sign-in URL
+  sign_in_url, state = get_sign_in_url()
+  # Save the expected state so we can validate in the callback
+  request.session['auth_state'] = state
+  # Redirect to the Azure sign-in page
+  return HttpResponseRedirect(sign_in_url)
+
+def callback(request):
+  # Get the state saved in session
+  expected_state = request.session.pop('auth_state', '')
+  # Make the token request
+  token = get_token_from_code(request.get_full_path(), expected_state)
+
+  # Get the user's profile
+  user = get_user(token)
+
+  # Save token and user
+  store_token(request, token)
+  store_user(request, user)
+
+  return HttpResponseRedirect(reverse('home'))
+
+
+
+'''
+def calendar(request):
+  context = initialize_context(request)
+
+  token = get_token(request)
+
+  events = get_calendar_events(token)
+
+  if events:
+    # Convert the ISO 8601 date times to a datetime object
+    # This allows the Django template to format the value nicely
+    for event in events['value']:
+      event['start']['dateTime'] = dateutil.parser.parse(event['start']['dateTime'])
+      event['end']['dateTime'] = dateutil.parser.parse(event['end']['dateTime'])
+
+    context['events'] = events['value']
+
+  return render(request, 'teams/calendar.html', context)
+'''
